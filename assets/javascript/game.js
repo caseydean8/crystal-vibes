cab2ed934604ea775845ac364cf606acd7e1c31c
Casey Carroll <caseydean8@gmail.com>
2019-06-12 10:54:47 -0400

    Added win/loss counter functionality.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,6 +3,8 @@ let wins = 0;
 
 let losses = 0;
 
+// let total =0; as global it messes with buttons
+
 let crys1 = 0;
 
 let crys2 = 0;
@@ -15,40 +17,96 @@ let crys4 = 0;
 
 let score = 0;
 
+var singletonDesign = false
+
+
+// Create a reset function that logs wins and losses 
+function reset() {
+    score = 0;
+    $("#score").html(0); 
+    startLogic();
+}
+
 // create a function that holds five? functions for generating the random numbers required for game.
 function startLogic() {
     //create a total value between 19 and 120 using Math.random.
         let min = 19; 
         let max = 120  
-        let total = Math.floor(Math.random() * (+max - +min)) + +min; console.log(total); 
+        let total = Math.floor(Math.random() * (+max - +min)) + +min; 
         // Outputs total to the screen.
-        $("#total").html(total);
+        $("#total-html").html(total);
+        console.log(total + " under random generator")
         //Create separate values for 4 crystals.
         let crysMin = 1;
         let crysMax = 12;
         crys1 = Math.floor(Math.random() * (+crysMax - +crysMin)) + +crysMin;
-        console.log(crys1);
+        console.log(crys1, "button 1");
         crys2 = Math.floor(Math.random() * (+crysMax - +crysMin)) + +crysMin;
-        console.log(crys2);
+        console.log(crys2, "button 2");
         crys3 = Math.floor(Math.random() * (+crysMax - +crysMin)) + +crysMin;
-        console.log(crys3);
+        console.log(crys3, "button 3");
         crys4 = Math.floor(Math.random() * (+crysMax - +crysMin)) + +crysMin;
-        console.log(crys4);
+        console.log(crys4, "button 4");
+
+              // Utilize click events to add crystal values to player score.
+    $("#crystal-1").off().on("click", function (){
+    $("#score-html").html(score += crys1);
+    console.log(score, total);
+    if (score >= total){
+        console.log(score, total , " under if statement btn 1")
+        scoring();
+        }
+    })
+
+    $("#crystal-2").off().on("click", function (){
+    $("#score-html").html(score += crys2);
+    if (score >= total){
+        console.log(score, total , " under if statement btn 2")
+        scoring();
+        }
+})
+
+    $("#crystal-3").off().on("click", function (){
+    $("#score-html").html(score += crys3);
+    if (score >= total){
+        console.log(score, total , " under if statement btn 3")
+        scoring();
+        }
+})
+
+    $("#crystal-4").off().on("click", function (){
+    $("#score-html").html(score += crys4);
+    if (score >= total){
+        console.log(score, total , " under if statement btn 4")
+        scoring();
+        }
+})
+
+// setting singleton, not sure why...
+
+function scoring(){
+    if (score === total) {
+        singletonDesign =true
+    }
+    if (singletonDesign) {
+        console.log(score);
+        wins++;
+        $("#wins").html("Wins " + wins);
+        // score =0;
+        reset();
+    }
+    else if (score > total) {
+        // singletonDesign = true
+        losses++;
+        $("#losses").html("Losses " + losses);
+        score = 0;
+        reset();
+    }
 }
 
-// Utilize click events to add crystal values to player score.
-$("#crystal-1").click(function (){
-    $("#score").html(score += crys1);
-})
+}
 
-$("#crystal-2").click(function (){
-    $("#score").html(score += crys2);
-})
+  
 
-$("#crystal-3").click(function (){
-    $("#score").html(score += crys3);
-})
+        
 
-$("#crystal-4").click(function (){
-    $("#score").html(score += crys4);
-})
