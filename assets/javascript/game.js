c4c34b8f0083c876fcc7bdcfddece5663f966168
Casey Carroll <caseydean8@gmail.com>
2019-06-12 12:06:22 -0400

    Cleaned up code to make it more readable.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,54 +1,42 @@
 // Begin by assigning variables. I'm using let because because the varibles will have different values assigned to them
-let wins = 0;
+var wins = 0;
 
-let losses = 0;
+var losses = 0;
 
-// let total =0; as global it messes with buttons
-
-let crys1 = 0;
-
-let crys2 = 0;
-
-let crys3 = 0;
-
-let crys4 = 0;
-
-// let total = 0;
-
-let score = 0;
+var score = 0;
 
 var singletonDesign = false
 
 
 // Create a reset function that logs wins and losses 
 function reset() {
-    score = 0;
-    $("#score").html(0); 
+    // score = 0;
+    $("#score").html(score); 
     startLogic();
 }
 
 // create a function that holds five? functions for generating the random numbers required for game.
+//create a total value between 19 and 120 using Math.random.
 function startLogic() {
-    //create a total value between 19 and 120 using Math.random.
-        let min = 19; 
-        let max = 120  
-        let total = Math.floor(Math.random() * (+max - +min)) + +min; 
-        // Outputs total to the screen.
-        $("#total-html").html(total);
-        console.log(total + " under random generator")
-        //Create separate values for 4 crystals.
-        let crysMin = 1;
-        let crysMax = 12;
-        crys1 = Math.floor(Math.random() * (+crysMax - +crysMin)) + +crysMin;
-        console.log(crys1, "button 1");
-        crys2 = Math.floor(Math.random() * (+crysMax - +crysMin)) + +crysMin;
-        console.log(crys2, "button 2");
-        crys3 = Math.floor(Math.random() * (+crysMax - +crysMin)) + +crysMin;
-        console.log(crys3, "button 3");
-        crys4 = Math.floor(Math.random() * (+crysMax - +crysMin)) + +crysMin;
-        console.log(crys4, "button 4");
+    let min = 19; 
+    let max = 120  
+    let total = Math.floor(Math.random() * (+max - +min)) + +min; 
+    // Outputs total to the screen.
+    $("#total-html").html(total);
+    console.log(total + " under random generator")
+    //Create separate values for 4 crystals.
+    let crysMin = 1;
+    let crysMax = 12;
+    crys1 = Math.floor(Math.random() * (+crysMax - +crysMin)) + +crysMin;
+    console.log(crys1, "button 1");
+    crys2 = Math.floor(Math.random() * (+crysMax - +crysMin)) + +crysMin;
+    console.log(crys2, "button 2");
+    crys3 = Math.floor(Math.random() * (+crysMax - +crysMin)) + +crysMin;
+    console.log(crys3, "button 3");
+    crys4 = Math.floor(Math.random() * (+crysMax - +crysMin)) + +crysMin;
+    console.log(crys4, "button 4");
 
-              // Utilize click events to add crystal values to player score.
+    // Utilize click events to add crystal values to player score.
     $("#crystal-1").off().on("click", function (){
     $("#score-html").html(score += crys1);
     console.log(score, total);
@@ -83,7 +71,6 @@ function startLogic() {
 })
 
 // setting singleton, not sure why...
-
 function scoring(){
     if (score === total) {
         singletonDesign =true
@@ -92,7 +79,7 @@ function scoring(){
         console.log(score);
         wins++;
         $("#wins").html("Wins " + wins);
-        // score =0;
+        score =0;
         reset();
     }
     else if (score > total) {
