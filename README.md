6437495658b43a8a5001a54fda14d661409cc2a1
Casey Carroll <caseydean8@gmail.com>
2019-06-14 10:14:08 -0400

    Read me added and js code cleaned up.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # unit-4-game
+This project uses javascript and jQuery to render a number guessing game. 
+
+A random number between 19 and 120 is displayed, and a random number between 1 and 12 is assigned to each of four crystal buttons but is not displayed.
+
+When a player clicks on a button, the value is added to a score below the buttons and if the player can match the random number displayed, a win is recorded. 
+If the player goes over a loss is recorded.
+
+Button values are displayed in the console for testing.
+
+Any questions or comments can be sent to caseydean8@gmail.com.
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,48 +1,44 @@
-// Begin by assigning variables. I'm using let because because the varibles will have different values assigned to them
+// Global variables assigned.
 var wins = 0;
 
 var losses = 0;
 
 var score = 0;
 
-var singletonDesign = false
-
-
-// Create a reset function that logs wins and losses 
+// Create a reset function.
 function reset() {
     score = 0;
-    $("#score").html(score); 
     startLogic();
 }
 
-// create a function that holds five? functions for generating the random numbers required for game.
-//create a total value between 19 and 120 using Math.random.
+// Create a function that holds five random number generators.
 function startLogic() {
+    
+    //Create a total value between 19 and 120 using Math.random.
     let min = 19; 
     let max = 120  
     let total = Math.floor(Math.random() * (+max - +min)) + +min; 
+    
     // Outputs total to the screen.
     $("#total-html").html(total);
-    console.log(total + " under random generator")
+
     //Create separate values for 4 crystals.
     let crysMin = 1;
     let crysMax = 12;
     crys1 = Math.floor(Math.random() * (+crysMax - +crysMin)) + +crysMin;
-    console.log(crys1, "button 1");
+    console.log(crys1, "yellow button");
     crys2 = Math.floor(Math.random() * (+crysMax - +crysMin)) + +crysMin;
-    console.log(crys2, "button 2");
+    console.log(crys2, "blue button");
     crys3 = Math.floor(Math.random() * (+crysMax - +crysMin)) + +crysMin;
-    console.log(crys3, "button 3");
+    console.log(crys3, "green button");
     crys4 = Math.floor(Math.random() * (+crysMax - +crysMin)) + +crysMin;
-    console.log(crys4, "button 4");
+    console.log(crys4, "purple button");
 
     // Utilize click events to add crystal values to player score.
     $("#crystal-1").off().on("click", function (){
     $("#score-html").html(score += crys1);
     $("#heading").css("color", "yellow");
-    console.log(score, total);
     if (score >= total){
-        console.log(score, total , " under if statement btn 1")
         scoring();
         }
     })
@@ -51,51 +47,42 @@ function startLogic() {
     $("#score-html").html(score += crys2);
     $("#heading").css("color", "blue");
     if (score >= total){
-        console.log(score, total , " under if statement btn 2")
         scoring();
         }
-})
+    })
 
     $("#crystal-3").off().on("click", function (){
     $("#heading").css("color", "green");
     $("#score-html").html(score += crys3);
     if (score >= total){
-        console.log(score, total , " under if statement btn 3")
         scoring();
         }
-})
+    })
 
     $("#crystal-4").off().on("click", function (){
     $("#heading").css("color", "purple");
     $("#score-html").html(score += crys4);
     if (score >= total){
-        console.log(score, total , " under if statement btn 4")
         scoring();
         }
-})
+    })
 
-// setting singleton, not sure why...
-function scoring(){
+    // Win/loss function that directs game to reset.
+    function scoring(){
     if (score === total) {
-        singletonDesign =true
-    }
-    if (singletonDesign) {
-        console.log(score);
-        wins++;
+        wins++
         $("#wins").html("Wins " + wins);
         $("#score-html").text(0);
-        // score = 0;
         reset();
-    }
+        }
+
     else if (score > total) {
-        // singletonDesign = true
         losses++;
         $("#losses").html("Losses " + losses);
         $("#score-html").text(0);
-        // score = 0;
         reset();
+        }
     }
-}
 
 }
 
